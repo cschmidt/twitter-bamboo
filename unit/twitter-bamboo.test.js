5c78446634226d47c03b172beed0335d678c5b63
Carl Schmidt <carl.schmidt@gmail.com>
2018-07-10 05:13:33 +0000

    Started working on list merging logic

---
+++ unit/twitter-bamboo.test.js
@@ -0,0 +1,27 @@
+'use strict'
+/* global test expect */
+// import { extractScreenNames } from 'twitter-bamboo'
+var _ = require('lodash')
+
+test('parses screen names', () => {
+  let screenNames = ['screenName', '@screenName', 'http://twitter.com/screenName',
+    'https://twitter.com/screenName'
+  ]
+  for (const screenName of screenNames) {
+    expect(screenName.split(/\/|@/).pop()).toBe('screenName')
+  }
+  // expect(extractScreenNames([employee])).toBe([{ screenName: 'screenName' }])
+})
+
+test('indexes employees and list members by screen name', () => {
+  let employeesByScreenName = new Map()
+  employeesByScreenName.set('s1', { screenName: 's1' })
+  employeesByScreenName.set('s2', { screenName: 's2' })
+
+  let listMembersByScreenName = new Map()
+  listMembersByScreenName.set('s2', { screenName: 's2' })
+  listMembersByScreenName.set('s3', { screenName: 's3' })
+
+  console.log(_.difference(Array.from(employeesByScreenName.keys()), Array.from(listMembersByScreenName.keys())))
+  console.log(_.difference(Array.from(listMembersByScreenName.keys()), Array.from(employeesByScreenName.keys())))
+})
