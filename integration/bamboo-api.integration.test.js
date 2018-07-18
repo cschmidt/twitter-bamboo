707c4198ec65a0dea13a5b4dc50aa0e02c95d112
Carl Schmidt <carl.schmidt@gmail.com>
2018-07-18 19:58:20 +0000

    DRY up config loading

--- integration/bamboo-api.integration.test.js
+++ integration/bamboo-api.integration.test.js
@@ -1,20 +1,19 @@
 'use strict'
 /* global beforeAll test expect */
-const ssm = require('aws-ssm-params')
 // FIXME: ugh, is there an alternative to relative pathing?
+const { loadConfig } = require('../lib/util')
+const config = {}
 const Bamboo = require('../lib/bamboo-api')
-const config = require('../config.json')
 
 beforeAll(() => {
-  return ssm(config.ssm).then(function(params) {
-    config.bamboo.apiKey = params['bamboo-api-key']
+  return loadConfig().then(loaded_config => {
+    Object.assign(config, loaded_config)
   })
 })
 
-
 test('gets employee list from Bamboo', () => {
-  const Api = new Bamboo(config.bamboo)
-  return Api.employees().then(employees => {
+  const api = new Bamboo(config.bamboo)
+  return api.employees().then(employees => {
     expect(employees.length).toBeGreaterThan(0)
     expect(Object.keys(employees[0]))
       .toEqual(['id', 'firstName', 'lastName', 'twitterFeed', 'status'])
--- integration/twitter-api.integration.test.js
+++ integration/twitter-api.integration.test.js
@@ -1,11 +1,19 @@
 'use strict'
-/* global test expect */
-const config = require('../config.json')
-const Twitter = new(require('../lib/twitter-api'))(config.twitter)
+/* global test expect beforeAll*/
+const { loadConfig } = require('../lib/util')
+const config = {}
+const Twitter = require('../lib/twitter-api')
+
+beforeAll(() => {
+  return loadConfig().then(loaded_config => {
+    Object.assign(config, loaded_config)
+  })
+})
 
 
 test('gets list members from Twitter', () => {
-  return Twitter.listMembers().then(listMembers => {
+  let api = new Twitter(config.twitter)
+  return api.listMembers().then(listMembers => {
     expect(listMembers.length).toBeGreaterThan(0)
     for (const attributeName of ['id', 'name', 'screen_name']) {
       expect(listMembers[0][attributeName]).toBeTruthy()
