efce82ef5a94f92d244b5916eb2136b9e410572f
Carl Schmidt <carl.schmidt@gmail.com>
2018-07-18 00:07:50 +0000

    Remove logging, add some real assertions

--- integration/twitter-api.integration.test.js
+++ integration/twitter-api.integration.test.js
@@ -6,9 +6,9 @@ const Twitter = new(require('../lib/twitter-api'))(config.twitter)
 
 test('gets list members from Twitter', () => {
   return Twitter.listMembers().then(listMembers => {
-    // let { id, name, screen_name } = listMembers[0]
-    // console.log(id, name, screen_name)
-    console.log('Retrieved', listMembers.length, 'list members')
     expect(listMembers.length).toBeGreaterThan(0)
+    for (const attributeName of ['id', 'name', 'screen_name']) {
+      expect(listMembers[0][attributeName]).toBeTruthy()
+    }
   })
 })
