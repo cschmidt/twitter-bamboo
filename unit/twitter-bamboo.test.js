75bfdb6a23cdd9adf8b189d6313ad13fcbcf1ae9
Carl Schmidt <carl.schmidt@gmail.com>
2018-07-24 15:40:31 +0000

    Mocked out calls to Twitter and Bamboo APIs for integration tests

--- unit/twitter-bamboo.test.js
+++ unit/twitter-bamboo.test.js
@@ -1,8 +1,42 @@
 'use strict'
-/* global test expect beforeAll */
+/* global test expect beforeAll jest */
 const TwitterBamboo = require('../lib/twitter-bamboo')
 const config = require('../config.json')
 const ssm = require('aws-ssm-params')
+
+const sampleEmployees = [{
+  id: 'id1',
+  firstName: 'Leslie',
+  lastName: 'Collin',
+  twitterFeed: '@Lesamatron',
+  status: 'Active'
+}, {
+  id: 'id2',
+  firstName: 'Sheldi',
+  lastName: 'St. Amand',
+  twitterFeed: null,
+  status: 'Inactive'
+}]
+
+const sampleListMembers = [{
+  id: 3796643658,
+  name: 'Partho Ghosh',
+  screen_name: 'productpartho'
+}]
+
+function bambooApiMock() {
+  const mock = {}
+  mock.employees = jest.fn()
+  mock.employees.mockImplementation(() => Promise.resolve(sampleEmployees))
+  return mock
+}
+
+function twitterApiMock() {
+  const mock = {}
+  mock.listMembers = jest.fn()
+  mock.listMembers.mockImplementation(() => Promise.resolve(sampleListMembers))
+  return mock
+}
 
 beforeAll(() => {
   return ssm(config.ssm).then(params => {
@@ -22,4 +56,14 @@ test('parses screen names', () => {
 
 test('indexes employees and list members by screen name', () => {
   //FIXME: populate with a real test
+  const twitterBamboo = new TwitterBamboo(config)
+  twitterBamboo.bambooApi = bambooApiMock()
+  twitterBamboo.twitterApi = twitterApiMock()
+
+  return twitterBamboo.exec().then(twitterBamboo => {
+    expect(twitterBamboo.employeesByScreenName.size).toBe(1)
+    let leslie = twitterBamboo.employeesByScreenName.values().next().value
+    expect(leslie['twitterFeed']).toBe('@Lesamatron')
+    expect(twitterBamboo.listMembersByScreenName.size).toBe(1)
+  })
 })
